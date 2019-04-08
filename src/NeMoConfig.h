4c79f1da38888f7d0b81daa946991f09ea0b7ad1
Mark Plagge <plaggm@rpi.edu>
2019-04-08 17:49:01 -0400

    Still working - need to migrate for further tests

---
+++ src/NeMoConfig.h
@@ -0,0 +1,8 @@
+//
+// Created by plaggm on 4/8/19.
+//
+
+#ifndef NEMO2_NEMOCONFIG_H
+#define NEMO2_NEMOCONFIG_H
+
+#endif //NEMO2_NEMOCONFIG_H
---
+++ src/info_display.h
@@ -0,0 +1,8 @@
+//
+// Created by plaggm on 4/4/19.
+//
+
+#ifndef NEMO2_INFO_DESPLAY_H
+#define NEMO2_INFO_DESPLAY_H
+
+#endif //NEMO2_INFO_DESPLAY_H
